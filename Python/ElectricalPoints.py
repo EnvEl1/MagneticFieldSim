d6115722f63430f8fbd4d2ed5a60ee5d3c973c8c
EnvEl1 <aejacobs1433@gmail.com>
2025-10-14 22:13:01 -0700

    successfully made worldline for a single particle next steps: apply a second dimension currently only in one dimension

--- Python/ElectricalPoints.py
+++ Python/ElectricalPoints.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # constants
 g = 9.81 # m/s^2
@@ -6,20 +7,30 @@ permitivity = 8.854e-12 # C^2/(N*m^2)
 coulomb = 1.602e-19 # C
 electric_charge = 1 * coulomb # C
 
+# User inputs for point charge 1
 pointx_input = input("Enter x coordinate of point charge 1 (m): ")
-pointy_input= input("Enter y coordinate of point charge 1 (m): ")
-
-pointx2_input= input("Enter x coordinate of point charge 2 (m): ")
-pointy2_input= input("Enter y coordinate of point charge 2 (m): ")
+pointy_input = input("Enter y coordinate of point charge 1 (m): ")
+point_velocity_input = input("Enter velocity of point charge 1 (m/s): ")
 
 pointx = float(pointx_input)
 pointy = float(pointy_input)
+point_velocity = float(point_velocity_input)
 
-pointx2 = float(pointx2_input)
-pointy2 = float(pointy2_input)
+# time variables
+t_start = 0 # seconds
+t_end = 5 # seconds
+dt = 0.1 # seconds
+time = np.arange(t_start, t_end, dt)
 
-distance = np.sqrt((pointx2-pointx)**2 + (pointy2-pointy)**2)
-ForceE = (1/(4*np.pi*permitivity)) * (electric_charge**2/(distance**2))
+# position calculations
+x_position = pointx + point_velocity * time
+y_position = pointy + 0 * time  # assuming no vertical movement
 
-print(distance)
-print(ForceE)
+
+# visualitation graphing
+fig, ax = plt.subplots()
+plt.plot(x_position, y_position,linewidth=3,linestyle='--' , label='Point Charge 1 Position')
+plt.xlabel('X Position (m)')
+plt.ylabel('Y Position (m)')
+plt.grid()
+plt.show()
